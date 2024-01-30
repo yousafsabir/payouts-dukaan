284b5c551eae6fbaa8db3e1dd6f446bba4f43167
yousafsabir <myousafdev@gmail.com>
2024-01-30 17:01:11 +0500

    chore: remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 
-import RootDashboardLayout from '@/screens/dashboard/layout'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,7 +22,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<head></head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
