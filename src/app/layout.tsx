037061fca742c9e554f4350a4f8b9a626ba5a471
yousafsabir <myousafdev@gmail.com>
2024-01-30 17:00:27 +0500

    feat (ui): export themeColor from viewport instead of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 
 import RootDashboardLayout from '@/screens/dashboard/layout'
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
 	title: 'Payments - Dukaan',
 	description:
 		'A portfolio frontend project aimed to showcase my pixel-perfection skill. link to figma: https://t.co/0oUgtwer25',
+}
+
+export const viewport: Viewport = {
 	themeColor: '#146EB4',
 }
 
