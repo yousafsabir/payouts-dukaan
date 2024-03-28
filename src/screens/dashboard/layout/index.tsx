869b543b36098f899aa944bbab6eb35af9277fbc
yousafsabir <myousafdev@gmail.com>
2024-03-29 03:18:38 +0500

    fix (ui): main overflow fixed

--- src/screens/dashboard/layout/index.tsx
+++ src/screens/dashboard/layout/index.tsx
@@ -6,7 +6,7 @@ export default function RootDashboardLayout({ children }: { children?: React.Rea
 		<main className='relative flex min-h-screen'>
 			<DashboardSidebar />
 			<DashboardNavbar />
-			<main className='m-0 h-full flex-1 bg-app-gray-30 px-4 py-6 pt-[calc(32px+57px)] sm:p-8 sm:pt-[calc(32px+65px)] lg:ml-[224px]'>
+			<main className='m-0 h-full max-w-full flex-1 bg-app-gray-30 px-4 py-6 pt-[calc(32px+57px)] sm:p-8 sm:pt-[calc(32px+65px)] lg:ml-[224px]'>
 				{children}
 			</main>
 		</main>
