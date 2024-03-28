10795fa42153e445016cbc2b8dceda0400046fbd
yousafsabir <myousafdev@gmail.com>
2024-03-29 04:18:45 +0500

    feat: added useQueryParams() utility hook to update query param easily

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
 }
+
+import { useRouter, usePathname, useSearchParams } from 'next/navigation'
+
+export function useQueryParams() {
+	const router = useRouter()
+	const path = usePathname()
+	const params = useSearchParams()
+
+	function updateParam(key: string, value: string) {
+		const updatedSearchParams = new URLSearchParams(params.toString())
+		updatedSearchParams.set(key, value)
+		router.push(path + '?' + updatedSearchParams.toString())
+	}
+
+	return {
+		params,
+		updateParam,
+	}
+}
